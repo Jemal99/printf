c114d9dbb12b564735390379db5c6f925bb134b8
jemal <104770971+Jemal99@users.noreply.github.com>
2022-11-28 10:23:17 +0300

    Update main.c

--- test/main.c
+++ test/main.c
@@ -14,7 +14,7 @@ int main(void)
     unsigned int ui;
     void *addr;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
+    len = printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
